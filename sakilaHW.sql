32df634f4093fe6015f123918417c3c1486ab3c1
Larissa <110873588+larissacybyk@users.noreply.github.com>
2024-02-11 23:19:11 -0500

    Update sakilaHW.sql
    question 5

--- sakilaHW.sql
+++ sakilaHW.sql
@@ -5,3 +5,5 @@ select category.name, count(*) from category join film_category where category.c
 select rating, AVG(rental_duration) from film group by rating;
 
 select customer.first_name, customer.last_name, count(rental.rental_id) from customer join rental on customer.customer_id = rental.customer_id group by customer.customer_id;
+
+select customer.store_id, count(rental.rental_id) from customer join rental on customer.customer_id = rental.customer_id group by customer.store_id order by count(rental.rental_id) desc limit 1;
