b458726b622d85ad2a0eb7484da84d58a1388508
Larissa <110873588+larissacybyk@users.noreply.github.com>
2024-02-11 22:49:34 -0500

    Update sakilaHW.sql
    question 2

--- sakilaHW.sql
+++ sakilaHW.sql
@@ -1,2 +1,3 @@
 select actor.first_name, actor.last_name from actor join film_actor on actor.actor_id = film_actor.actor_id join film on film.film_id = film_actor.film_id where film.title = "ACADEMY DINOSAUR";
 
+select category.name, count(*) from category join film_category where category.category_id = film_category.category_id group by category.category_id;
