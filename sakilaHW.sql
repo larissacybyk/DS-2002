e3e7840f11b83c9790a15a519d9d07bba7b0f94a
Larissa <110873588+larissacybyk@users.noreply.github.com>
2024-02-11 22:42:05 -0500

    Update sakilaHW.sql
    question 1

--- sakilaHW.sql
+++ sakilaHW.sql
@@ -1,1 +1,2 @@
-use actors;
+select actor.first_name, actor.last_name from actor join film_actor on actor.actor_id = film_actor.actor_id join film on film.film_id = film_actor.film_id where film.title = "ACADEMY DINOSAUR";
+
