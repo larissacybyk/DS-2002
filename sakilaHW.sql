bca41735a69d8c6540a7f62a37b6789b7b412b8f
Larissa <110873588+larissacybyk@users.noreply.github.com>
2024-02-11 23:44:59 -0500

    Update sakilaHW.sql
    question 10

--- sakilaHW.sql
+++ sakilaHW.sql
@@ -15,3 +15,5 @@ select category.name, AVG(film.rental_rate) from film join film_category on film
 select film.title, max(rental.rental_date) from film left join inventory on film.film_id = inventory.film_id left join rental on inventory.inventory_id = rental.inventory_id where rental.rental_date < DATE_SUB(NOW(), INTERVAL 1 MONTH) group by film.title;
 
 select customer.first_name, customer.last_name, SUM(payment.amount) from customer join payment on customer.customer_id = payment.customer_id group by customer.customer_id;
+
+select language.name, AVG(film.length) from film join language on film.language_id = language.language_id group by language.name;
