8f623e84e3e94d97fe085e5f4e438d7192f08d45
larissacybyk <jdj9ng@virginia.edu>
2024-02-26 22:09:24 -0500

    Writing to json file

--- APICalls.py
+++ APICalls.py
@@ -1,27 +1,42 @@
 # pip install requests (in console)
 import requests
 import pandas as pd
+import json
 
-print("Enter 'e' to exit")
-df = pd.DataFrame([], columns=["Country", "Capital", "Population"])
 country = input("Please enter a country name: ")
-while (country != "e"):
-    country = country.strip().lower()
-    url = "https://restcountries.com/v3.1/name/" + country 
-    response = requests.request("GET", url)
-    json = response.json()
+country = country.strip()
+url = "https://restcountries.com/v3.1/name/" + country 
+response = requests.request("GET", url)
+json_data = response.json()
 
-    if response.status_code >= 400: 
-        print("Invalid input, please try again.")
-        exit()
+if response.status_code >= 400: 
+    print("Invalid input, please try again.")
+    exit()
 
-    cap = json[0]["capital"][0]
-    pop = json[0]["population"]
-    print("Capital: " + cap)
-    print("Population: " + str(pop))
+cap = json_data[0]["capital"][0]
+pop = json_data[0]["population"]
+print("Capital: " + cap)
+print("Population: " + str(pop))
 
-    data = [country.capitalize(), cap, pop]
-    df.loc[len(df.index)] = data
+data = [country.capitalize(), cap, pop]
+df = pd.DataFrame([data], columns=["country", "capital", "population"])
 
-    print(df)
-    country = input("Please enter a country name: ")
+print(df)
+
+# writing to json file
+# new_country = {"country": country, "capital": cap, "population": pop}
+
+new_country = df.to_dict(orient="records")[0]
+try:
+    file = open("APICalls.json", 'r+')
+except:
+    file = open("APICalls.json", 'a')
+try:
+    file_data = json.load(file)
+    file_data["countries"].append(new_country)
+    file.seek(0)
+    json.dump(file_data, file, indent=4)      
+except:
+    json_obj = {"countries":[new_country]}
+    json2 = json.dump(json_obj, file, indent=4)
+file.close()
