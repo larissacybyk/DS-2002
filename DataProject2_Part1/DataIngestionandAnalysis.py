91f55fd537bb093ea1901b797c3266591d32f5a6
larissacybyk <jdj9ng@virginia.edu>
2024-05-10 18:16:43 -0400

    part 1 done

--- DataProject2_Part1/DataIngestionandAnalysis.py
+++ DataProject2_Part1/DataIngestionandAnalysis.py
@@ -2,6 +2,7 @@ import time
 import requests
 import pandas as pd
 import json
+import sqlite3
 
 
 def api_call():
@@ -15,12 +16,24 @@ def api_call():
     return json_data
 
 def runbot():
+    #creation of SQLite database
+    conn = sqlite3.connect('data_ingestion.db')
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS data (factor integer, pi real, time text)")
     start_time = time.perf_counter()
-
-    while time.perf_counter() - start_time < 3600:
-        if (time.perf_counter() - start_time) % 60 == 0:
+    curr = int(time.perf_counter() - start_time)
+    while curr < 3600:
+        if curr % 60 == 0:
             #call api and update db
             json_data = api_call()
+            cursor.execute('''
+            INSERT OR IGNORE INTO data (factor, pi, time)
+            VALUES (?, ?, ?)
+        ''', (json_data['factor'], json_data['pi'], json_data['time']))
+            conn.commit()
+        time.sleep(1)
+        curr = int(time.perf_counter() - start_time)
+            
 
 
 # running
